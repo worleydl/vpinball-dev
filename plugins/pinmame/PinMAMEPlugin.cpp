e86c1311d68bb1f794ca4d746d3dacce5c8ecc19
= <danworley@gmail.com>
2025-11-14 10:33:19 -0500

    wine-on-win: fix pinmame path bandaid

--- plugins/pinmame/PinMAMEPlugin.cpp
+++ plugins/pinmame/PinMAMEPlugin.cpp
@@ -419,7 +419,7 @@ MSGPI_EXPORT void MSGPIAPI PinMAMEPluginLoad(const uint32_t sessionId, const Msg
          #else
             // FIXME implement a last resort or just ask the user to define its path setup in the settings ?
             // DLW: quick hack, use local path ./pinmame
-            pinmamePath = "./pinmame/";
+            pinmamePath = "C:\\dev\\std\\vpinball\\pinmame\\";
             //LOGE("PinMAME path is not defined.");
          #endif
       }
