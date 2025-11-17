5ae57e7c94297d83d66aeeca640bea1bf68d12f5
= <danworley@gmail.com>
2025-11-17 11:34:08 -0500

    uwp: get gamepad events firing

--- uwp/main.cpp
+++ uwp/main.cpp
@@ -1,8 +1,12 @@
 #include <Windows.h>
+#include "SDL3/SDL.h"
 #include "SDL3/SDL_main.h"
 
 int bootstrap(int, char**)
 {
+   // todo: This should be forced in the SDL3-UWP build
+   SDL_SetHint(SDL_HINT_JOYSTICK_WGI, "true");
+
    char* args[] = {
 	  "himom.exe",
 	  "-play",
@@ -13,5 +17,5 @@ int bootstrap(int, char**)
 }
 
 int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR argv, int argc) {
-	SDL_RunApp(0, 0, bootstrap, NULL);
+   SDL_RunApp(0, 0, bootstrap, NULL);
 }
